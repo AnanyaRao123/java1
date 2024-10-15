552ad77d5faae2c37c7466da0bd510008f1930bb
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2024-10-15 15:41:43 +0530

    Max and Min value of byte, short, int...

---
+++ DefaultValue.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class DefaultValue {
+    public static void main(String[] args){
+        byte b1=0;
+        short s1=0;
+        int n1=0;
+        long l1=0;
+        System.out.println("MAX value of");
+        System.out.println("Byte:"+Byte.MAX_VALUE);
+        System.out.println("Short:"+Short.MAX_VALUE);
+        System.out.println("Integer:"+Integer.MAX_VALUE);
+        System.out.println("Long:"+Long.MAX_VALUE);
+        System.out.println("Float:"+Float.MAX_VALUE);
+        System.out.println("Double:"+Double.MAX_VALUE);
+        System.out.println();
+        System.out.println("MIN value of");
+        System.out.println("Byte:"+Byte.MIN_VALUE);
+        System.out.println("Short:"+Short.MIN_VALUE);
+        System.out.println("Integer:"+Integer.MIN_VALUE);
+        System.out.println("Long:"+Long.MIN_VALUE);
+        System.out.println("Float:"+Float.MIN_VALUE);
+        System.out.println("Double:"+Double.MIN_VALUE);
+        System.out.println();
+    }
+}
