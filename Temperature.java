91ee9427196e266f92ff094a32dc9495b786d78a
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2024-10-15 15:39:22 +0530

    Celsius to Fahrenheit

---
+++ Temperature.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class Temperature {
+    public static void main(String[] args) {
+        float tempC,tempF;
+        tempC=15;
+        tempF=((tempC*9)/5)+32;
+        System.out.println("Temperature is "+tempF);
+    }
+}
