285573840ac725f72796cfc8e0a91c4153e965c6
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2024-11-13 20:52:05 +0530

    reverse() method to reverse a string

---
+++ A23.java
@@ -0,0 +1,16 @@
+public class A23 {
+	public static void reverse(String s1) {
+		String s2="";
+		int len1=s1.length();
+		for(int i=0;i<len1;i++) {
+			s2=s2+s1.substring(len1-(i+1),len1-i);
+		}
+        	System.out.println(s2);
+	}
+
+	public static void main(String[] args) {
+		reverse("Arpitha");
+		reverse("Kavana");
+		reverse("Chandrashekar Rao Kuthyar");		
+	}
+}
