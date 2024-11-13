a9f6a12bd8b4fd0d1ac117dc0bdb828fd72b0af6
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2024-11-13 20:39:43 +0530

    Reversing a string

---
+++ A22.java
@@ -0,0 +1,11 @@
+public class A22 {
+	public static void main(String[] args) {
+		String s1="Thrisha S Bhat";
+		int len1=s1.length();
+		String s2=" ";
+		for(int i=0;i<len1;i++){
+			s2=s2+s1.substring(len1-(i+1),len1-i);
+		}
+				System.out.println(s2);
+	}
+}
